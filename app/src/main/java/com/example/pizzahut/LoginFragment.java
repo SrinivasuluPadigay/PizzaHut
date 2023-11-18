342639c93f6db109386d7ee37039d5298047010f
SrinivasuluPadigay <padigesrinivasulu@gmail.com>
2023-11-18 15:39:05 -0500

    Initial commit

---
+++ app/src/main/java/com/example/pizzahut/LoginFragment.java
@@ -0,0 +1,37 @@
+package com.example.pizzahut;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.pizzahut.databinding.FragmentLoginBinding;
+
+public class LoginFragment extends Fragment {
+
+    FragmentLoginBinding binding;
+
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentLoginBinding.inflate(inflater, container, false);
+        binding.buttonLogin.setO
+
+
+        return binding.getRoot();
+    }
+}
