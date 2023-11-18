bc900c8f21b72a74584d0d7f5cfb37f761b5a623
SrinivasuluPadigay <padigesrinivasulu@gmail.com>
2023-11-18 18:26:53 -0500

    created login and register fragment

--- app/src/main/java/com/example/pizzahut/MainActivity.java
+++ app/src/main/java/com/example/pizzahut/MainActivity.java
@@ -4,15 +4,49 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity implements LoginFragment.LoginFragmentListener, RegisterFragment.RegisterFragmentListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.root, new LoginFragment())
+                    .commit();
+        } else {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.root, new MenuFragment())
+                    .commit();
+        }
+    }
+
+    public void createNewAccount() {
         getSupportFragmentManager().beginTransaction()
-                .add(R.id.root, new LoginFragment())
+                .replace(R.id.root, new RegisterFragment())
+                .commit();
+    }
+
+    public void login() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.root, new LoginFragment())
+                .commit();
+    }
+
+    public void authSuccessful() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.root, new MenuFragment())
+                .commit();
+    }
+
+    public void logout() {
+        FirebaseAuth.getInstance().signOut();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.root, new LoginFragment())
                 .commit();
     }
 }
--- app/src/main/java/com/example/pizzahut/RegisterFragment.java
+++ app/src/main/java/com/example/pizzahut/RegisterFragment.java
@@ -1,64 +1,136 @@
 package com.example.pizzahut;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RegisterFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+import com.example.pizzahut.databinding.FragmentLoginBinding;
+import com.example.pizzahut.databinding.FragmentRegisterBinding;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class RegisterFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    FragmentRegisterBinding binding;
 
     public RegisterFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RegisterFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RegisterFragment newInstance(String param1, String param2) {
-        RegisterFragment fragment = new RegisterFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        binding = FragmentRegisterBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    // FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.buttonCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.login();
+            }
+        });
+
+        binding.buttonSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String first_name = binding.editTextFirstName.getText().toString();
+                String last_name = binding.editTextLastName.getText().toString();
+                String email = binding.editTextEmail.getText().toString();
+                String phone_number = binding.editTextPhone.getText().toString();
+                String password = binding.editTextPassword.getText().toString();
+
+                if (first_name.isEmpty()) {
+                    Toast.makeText(getActivity(), "Enter valid first name!", Toast.LENGTH_SHORT).show();
+                } else if (last_name.isEmpty()) {
+                    Toast.makeText(getActivity(), "Enter valid last name!", Toast.LENGTH_SHORT).show();
+                } else if (email.isEmpty()) {
+                    Toast.makeText(getActivity(), "Enter valid email!", Toast.LENGTH_SHORT).show();
+                } else if (phone_number.isEmpty()) {
+                    Toast.makeText(getActivity(), "Enter valid phone number!", Toast.LENGTH_SHORT).show();
+                } else if (password.isEmpty()) {
+                    Toast.makeText(getActivity(), "Enter valid password!", Toast.LENGTH_SHORT).show();
+                }else {
+                    mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+
+                                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                                UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                        .setDisplayName(first_name + " " + last_name)
+                                        .build();
+
+                                user.updateProfile(profileUpdates).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()){
+                                            Toast.makeText(getActivity(), "Sign-Up Successful",
+                                                    Toast.LENGTH_SHORT).show();
+                                            mListener.authSuccessful();
+                                        }else{
+                                            Toast.makeText(getActivity(), task.getException().getLocalizedMessage(),
+                                                    Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                });
+
+
+                            }else {
+                                Toast.makeText(getActivity(), task.getException().getLocalizedMessage(),
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+    RegisterFragmentListener mListener;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mListener = (RegisterFragmentListener) context;
+    }
+
+
+    interface RegisterFragmentListener {
+        void login();
+        void authSuccessful();
     }
 }
