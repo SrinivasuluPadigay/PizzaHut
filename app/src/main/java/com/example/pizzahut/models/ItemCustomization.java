04ebbc5a298053f6ec427bc43b9e682d2d420dee
SrinivasuluPadigay <padigesrinivasulu@gmail.com>
2023-11-21 19:42:57 -0500

    Added: Design for Menu Screen

---
+++ app/src/main/java/com/example/pizzahut/models/ItemCustomization.java
@@ -0,0 +1,12 @@
+package com.example.pizzahut.models;
+
+import java.io.Serializable;
+
+public class ItemCustomization implements Serializable {
+    public int size;
+    public String crust;
+    public String cheese_quantity;
+    public String sauce;
+    public String sauce_quantity;
+
+}
